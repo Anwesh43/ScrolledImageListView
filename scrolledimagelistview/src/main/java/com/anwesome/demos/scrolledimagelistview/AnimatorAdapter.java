4288e06f28328bf68eae93d24a1bece494a5b1f3
Anwesh43 <anweshthecool0@gmail.com>
2017-03-24 19:55:29 +0530

    Adding AnimatorAdapter

---
+++ scrolledimagelistview/src/main/java/com/anwesome/demos/scrolledimagelistview/AnimatorAdapter.java
@@ -0,0 +1,25 @@
+package com.anwesome.demos.scrolledimagelistview;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+
+/**
+ * Created by anweshmishra on 24/03/17.
+ */
+public class AnimatorAdapter implements ValueAnimator.AnimatorUpdateListener,Animator.AnimatorListener {
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+    }
+    public void onAnimationEnd(Animator animator) {
+
+    }
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+    public void onAnimationCancel(Animator animator) {
+
+    }
+    public void onAnimationStart(Animator animator) {
+
+    }
+}
